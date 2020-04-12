ca254bf06f0ddbb8985cdc1e9e1431122b06c428
andyMonterrubio <andy.mont18@gmail.com>
2020-04-11 23:16:17 -0500

    exercises for the basis of Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent, DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
